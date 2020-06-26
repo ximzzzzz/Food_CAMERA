e57f3db2669083d7d1d00ea6bdd965c7ff9cfd0d
ximzzzzz <ximzzzzz@gmail.com>
2020-06-26 18:50:34 +0900

    scatter model , ctc modify

--- SCATTER/scatter_model.py
+++ SCATTER/scatter_model.py
@@ -41,9 +41,13 @@ class SCATTER(nn.Module):
                                                   i_r_size= (opt.imgH, opt.imgW), i_channel_num=opt.input_channel, device = device)
         
         #Extract
-#         self.Extract = Extract.RCNN_extractor(opt.input_channel, opt.output_channel)
-#         self.Extract = Extract.ResNet_FeatureExtractor(opt.input_channel, opt.output_channel)
-        self.Extract = Extract.EfficientNet(opt)
+        if self.opt.extract =='RCNN':
+            self.Extract = self.Extract = Extract.RCNN_extractor(opt.input_channel, opt.output_channel)
+        elif 'efficientnet' in self.opt.extract :
+            self.Extract = Extract.EfficientNet(opt)
+        else:
+            raise print('invalid extract model name!')
+        #         self.Extract = Extract.ResNet_FeatureExtractor(opt.input_channel, opt.output_channel)
         self.FeatureExtraction_output = opt.output_channel # (imgH/16 -1 )* 512
         self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None,1)) # imgH/16-1   ->  1
             
@@ -52,26 +56,33 @@ class SCATTER(nn.Module):
                                               in_channels = self.FeatureExtraction_output, out_channels=1, stride=1)
         
         # CTC DECODER
-#         self.CTC = CTC.CTC_decoder(opt.output_channel, opt.output_channel, opt.num_classes, opt, device)
+        self.CTC = CTC.CTC_decoder(opt.output_channel, opt.output_channel, opt.num_classes, device)
             
         # Selective Contextual Refinement Block
-        self.SCR_1 = SCR.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
-                                                         hidden_size = int(self.FeatureExtraction_output/2),
-                                                        output_size = self.FeatureExtraction_output,
-                                                        num_classes = opt.num_classes, decoder_fix = False, device = device, 
-                                                        batch_max_length = opt.batch_max_length)
+#         self.SCR_1 = SCR.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
+#                                                          hidden_size = int(self.FeatureExtraction_output/2),
+#                                                         output_size = self.FeatureExtraction_output,
+#                                                         num_classes = opt.num_classes, decoder_fix = False, device = device, 
+#                                                         batch_max_length = opt.batch_max_length)
         
-        self.SCR_2 = SCR.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
-                                                         hidden_size = int(self.FeatureExtraction_output/2),
-                                                        output_size = self.FeatureExtraction_output,
-                                                        num_classes = opt.num_classes, decoder_fix = False, device = device,
-                                                        batch_max_length = opt.batch_max_length)
+#         self.SCR_2 = SCR.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
+#                                                          hidden_size = int(self.FeatureExtraction_output/2),
+#                                                         output_size = self.FeatureExtraction_output,
+#                                                         num_classes = opt.num_classes, decoder_fix = False, device = device,
+#                                                         batch_max_length = opt.batch_max_length)
         
-        self.SCR_3 = SCR.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
+#         self.SCR_3 = SCR.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
+#                                                          hidden_size = int(self.FeatureExtraction_output/2),
+#                                                         output_size = self.FeatureExtraction_output,
+#                                                         num_classes = opt.num_classes, decoder_fix = True, device = device,
+#                                                         batch_max_length = opt.batch_max_length)
+
+        self.SCR = SCR.SCR_Blocks(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        num_classes = opt.num_classes, decoder_fix = True, device = device,
-                                                        batch_max_length = opt.batch_max_length)
+                                                        num_classes = opt.num_classes, device = device,
+                                                        batch_max_length = opt.batch_max_length, 
+                                                        n_blocks=opt.scr_n_blocks)
   
     def forward(self, input, text, is_train):
         # Trans stage
@@ -82,16 +93,17 @@ class SCATTER(nn.Module):
         
         # Visual Feature Refinement
         visual_refined = self.VFR(visual_feature) # visual_ refined output Size([192, 23, 512])
-        
         # CTC DECODER
-#         ctc_prob  = self.CTC(visual_refined, text, opt)
-        
+        ctc_prob  = self.CTC(visual_refined, text)
         #Selective Contextual Refinement
-        scr_probs_1, H = self.SCR_1(visual_feature.permute(0, 3, 1, 2).squeeze(3), text, is_train)
-        scr_probs_2, H = self.SCR_2(H, text, is_train)
-        scr_probs_3, H = self.SCR_3(H, text, is_train)
-        
+#         scr_probs_1, H = self.SCR_1(visual_feature.permute(0, 3, 1, 2).squeeze(3), text, is_train)
+#         scr_probs_2, H = self.SCR_2(H, text, is_train)
+#         scr_probs_3, H = self.SCR_3(H, text, is_train)
+        prob_list = []
+        prob_list.append(ctc_prob)
+        Prob_list = self.SCR([visual_feature.permute(0, 3, 1, 2).squeeze(3), prob_list,  text, is_train])
 
-        return [scr_probs_1, scr_probs_2, scr_probs_3]
+#         return [scr_probs_1, scr_probs_2, scr_probs_3]
 #         return visual_refined, _
+        return Prob_list
     
