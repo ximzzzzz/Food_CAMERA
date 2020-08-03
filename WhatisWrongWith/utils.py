e113cddf0f7d792e2d137f0a344b40756dbff9dd
ximzzzzz <ximzzzzz@gmail.com>
2020-08-03 18:49:04 +0900

    cbloss

--- WhatisWrongWith/utils.py
+++ WhatisWrongWith/utils.py
@@ -21,10 +21,129 @@ from itertools import islice, cycle
 import cv2
 import torch.nn.functional as F
 from nltk.metrics.distance import edit_distance
-
+import re
+import pandas as pd
 
 device = torch.device('cuda')
 # device = torch.device('cpu')
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+
+def ClassCounter(data, position, converter):
+    if position =='top':
+        pos_idx = 1
+    elif position =='mid':
+        pos_idx = 2
+    elif position =='bot':
+        pos_idx = 3
+    
+    cls_stat = []
+    for label in np.asarray(data)[:,pos_idx]:
+        cls_stat.extend(label)
+    
+    cls_cnt = np.unique(cls_stat, return_counts=True)
+    cls_cnt = pd.DataFrame({'class' : cls_cnt[0], 'count' : cls_cnt[1]})
+    max_cnt = cls_cnt['count'].max()
+    
+    cnt_dict = {}
+    for cls in converter.dict.keys():
+        n_cls = cls_cnt[cls_cnt['class'] == cls]['count'].values
+        if len(n_cls)==0:
+            n_cls = [max_cnt]
+        cls_enc = converter.dict[cls]
+#         print(f'{cls} : {n_cls[0]}')
+        cnt_dict[cls_enc] = n_cls[0]
+        
+    return cnt_dict
+
+def reduced_focal_cbloss(labels, logits, alpha, gamma, threshold):
+    
+    BCLoss = F.binary_cross_entropy_with_logits(input = logits, target = labels,reduction = "none")
+    p = labels*torch.sigmoid(logits)+(1-labels)*(1-torch.sigmoid(logits))
+    modulator = torch.where(p < threshold, torch.ones_like(p), ((1-p)/threshold)**gamma)
+    loss = modulator * BCLoss
+
+#     weighted_loss = alpha * loss
+    focal_loss = torch.sum(loss)
+
+    focal_loss /= torch.sum(labels)
+    return focal_loss    
+
+
+def focal_loss(labels, logits, alpha, gamma):
+    """Compute the focal loss between `logits` and the ground truth `labels`.
+    Focal loss = -alpha_t * (1-pt)^gamma * log(pt)
+    where pt is the probability of being classified to the true class.
+    pt = p (if true class), otherwise pt = 1 - p. p = sigmoid(logit).
+    Args:
+      labels: A float tensor of size [batch, num_classes].
+      logits: A float tensor of size [batch, num_classes].
+      alpha: A float tensor of size [batch_size]
+        specifying per-example weight for balanced cross entropy.
+      gamma: A float scalar modulating loss from hard and easy examples.
+    Returns:
+      focal_loss: A float32 scalar representing normalized total loss.
+    """    
+    BCLoss = F.binary_cross_entropy_with_logits(input = logits, target = labels,reduction = "none")
+
+    if gamma == 0.0:
+        modulator = 1.0
+    else:
+        modulator = torch.exp(-gamma * labels * logits - gamma * torch.log(1 + torch.exp(-1.0 * logits)))
+
+    loss = modulator * BCLoss
+
+    weighted_loss = alpha * loss
+    focal_loss = torch.sum(weighted_loss)
+
+    focal_loss /= torch.sum(labels)
+    return focal_loss
+
+
+
+def CB_loss(labels, logits, samples_per_cls, no_of_classes, loss_type, beta, gamma, threshold=0.5):
+    """Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
+    Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
+    where Loss is one of the standard losses used for Neural Networks.
+    Args:
+      labels: A int tensor of size [batch].
+      logits: A float tensor of size [batch, no_of_classes].
+      samples_per_cls: A python list of size [no_of_classes].
+      no_of_classes: total number of classes. int
+      loss_type: string. One of "sigmoid", "focal", "softmax".
+      beta: float. Hyperparameter for Class balanced loss.
+      gamma: float. Hyperparameter for Focal loss.
+    Returns:
+      cb_loss: A float tensor representing class balanced loss
+    """
+    effective_num = 1.0 - np.power(beta, samples_per_cls)
+    weights = (1.0 - beta) / np.array(effective_num)
+    weights = weights / np.sum(weights) * no_of_classes
+
+    labels_one_hot = F.one_hot(labels, no_of_classes).float()
+
+    weights = torch.tensor(weights).float().to(device)
+    weights = weights.unsqueeze(0)
+    weights = weights.repeat(labels_one_hot.shape[0],1) * labels_one_hot
+    weights = weights.sum(1)
+    weights = weights.unsqueeze(1)
+    weights = weights.repeat(1,no_of_classes)
+
+    if loss_type == "focal":
+        cb_loss = focal_loss(labels_one_hot, logits, weights, gamma)
+    elif loss_type == "sigmoid":
+        cb_loss = F.binary_cross_entropy_with_logits(input = logits,target = labels_one_hot, weights = weights)
+    elif loss_type == "softmax":
+        pred = logits.softmax(dim = 1)
+        cb_loss = F.binary_cross_entropy(input = pred, target = labels_one_hot, weight = weights)
+    elif loss_type=='reduced_focal':
+        cb_loss =reduced_focal_cbloss(labels_one_hot, logits, weights, gamma, threshold)
+    return cb_loss
+
+
 
 def reduced_focal_loss(pred, target, ignore_index, alpha, gamma, threshold):
     ce_loss = torch.nn.functional.cross_entropy(pred, target, ignore_index=0, reduction='none')
@@ -65,7 +184,8 @@ def SaveDir_maker(base_model, base_model_dir = './models', ):
     directory = f'{base_model}_{mon}{day}/{trial_count}'
     empty_flag = True
     while empty_flag :
-        if (os.path.exists(os.path.join(base_model_dir, directory))) and (os.path.isfile(os.path.join(base_model_dir, directory, 'best_accuracy.pth'))):
+#         if (os.path.exists(os.path.join(base_model_dir, directory))) and (os.path.isfile(os.path.join(base_model_dir, directory, 'best_accuracy.pth'))):
+        if (os.path.exists(os.path.join(base_model_dir, directory))) and (bool(list(filter(re.compile('best_accuracy.*').match, os.listdir(os.path.join(base_model_dir, directory)))))):
             trial_count+=1
             directory = f'{base_model}_{mon}{day}/{trial_count}'
         else: 
