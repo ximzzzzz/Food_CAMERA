09f898eaca278fd168b391544ff71e2b09ddd4c3
ximzzzzz <ximzzzzz@gmail.com>
2020-09-21 18:45:58 +0900

    fucking ctc exclude

--- SCATTER/scatter_model_jamo.py
+++ SCATTER/scatter_model_jamo.py
@@ -36,6 +36,7 @@ importlib.reload(SCR_jamo)
 importlib.reload(CTC_jamo)
 importlib.reload(Extract)
 importlib.reload(utils)
+importlib.reload(scatter_utils)
 
 
 class SCATTER(nn.Module):
@@ -64,67 +65,68 @@ class SCATTER(nn.Module):
         self.VFR = VFR.Visual_Features_Refinement(kernel_size = (3,1), in_channels = self.FeatureExtraction_output, out_channels=1, stride=1)
         
         # CTC DECODER
-        self.CTC = CTC_jamo.CTC_decoder(opt.output_channel, opt.output_channel, opt.top_n_cls, opt.mid_n_cls, opt.bot_n_cls, device)
+        self.CTC = CTC_jamo.CTC_decoder(opt.output_channel, opt.output_channel, opt.top_n_cls_attn, opt.mid_n_cls_attn, opt.bot_n_cls_attn, device)
+                                                                                        ## pretrained model load 를 위해 임시로 attn cls 로 설정
             
         # Selective Contextual Refinement Block
         
         self.SCR_bot_1 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls], decoder_fix = False, device = device,
+                                                        vertical_num_classes = [opt.bot_n_cls_attn], decoder_fix = False, device = device,
                                                         batch_max_length = opt.batch_max_length)
                
         self.SCR_bot_2 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls], decoder_fix = False, device = device,
+                                                        vertical_num_classes = [opt.bot_n_cls_attn], decoder_fix = False, device = device,
                                                         batch_max_length = opt.batch_max_length)        
         
         self.SCR_bot_3 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls], decoder_fix = True, device = device,
+                                                        vertical_num_classes = [opt.bot_n_cls_attn], decoder_fix = True, device = device,
                                                         batch_max_length = opt.batch_max_length)       
         
         self.SCR_mid_1 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls, opt.mid_n_cls],
+                                                        vertical_num_classes = [opt.bot_n_cls_attn, opt.mid_n_cls_attn],
                                                         decoder_fix = False, device = device,
                                                         batch_max_length = opt.batch_max_length)
         
         self.SCR_mid_2 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls, opt.mid_n_cls],
+                                                        vertical_num_classes = [opt.bot_n_cls_attn, opt.mid_n_cls_attn],
                                                         decoder_fix = False, device = device,
                                                         batch_max_length = opt.batch_max_length)        
         
         self.SCR_mid_3 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls, opt.mid_n_cls],
+                                                        vertical_num_classes = [opt.bot_n_cls_attn, opt.mid_n_cls_attn],
                                                         decoder_fix = True, device = device,
                                                         batch_max_length = opt.batch_max_length)    
         
         self.SCR_top_1 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls, opt.mid_n_cls, opt.top_n_cls], 
+                                                        vertical_num_classes = [opt.bot_n_cls_attn, opt.mid_n_cls_attn, opt.top_n_cls_attn], 
                                                         decoder_fix = False, device = device, 
                                                         batch_max_length = opt.batch_max_length)
         
         self.SCR_top_2 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls, opt.mid_n_cls, opt.top_n_cls],
+                                                        vertical_num_classes = [opt.bot_n_cls_attn, opt.mid_n_cls_attn, opt.top_n_cls_attn],
                                                         decoder_fix = False, device = device,
                                                         batch_max_length = opt.batch_max_length)
         
         self.SCR_top_3 = SCR_jamo.Selective_Contextual_refinement_block(input_size = self.FeatureExtraction_output, 
                                                          hidden_size = int(self.FeatureExtraction_output/2),
                                                         output_size = self.FeatureExtraction_output,
-                                                        vertical_num_classes = [opt.bot_n_cls, opt.mid_n_cls, opt.top_n_cls],
+                                                        vertical_num_classes = [opt.bot_n_cls_attn, opt.mid_n_cls_attn, opt.top_n_cls_attn],
                                                         decoder_fix = True, device = device,
                                                         batch_max_length = opt.batch_max_length)
 
@@ -142,19 +144,21 @@ class SCATTER(nn.Module):
         # Extract stage
         visual_feature = self.Extract(input) # visual_feature.shape) # (192, 512, 1 , 23)
         
-        # Visual Feature Refinement
-        visual_refined = self.VFR(visual_feature) # visual_ refined output Size([192, 23, 512])
-        
         top_prob_list = []
         mid_prob_list = []
         bot_prob_list = []
         
-        # CTC DECODER
-        top_prob, mid_prob, bot_prob  = self.CTC(visual_refined)
+        if is_train:
         
-        top_prob_list.append(top_prob)
-        mid_prob_list.append(mid_prob)
-        bot_prob_list.append(bot_prob)
+            # Visual Feature Refinement
+            visual_refined = self.VFR(visual_feature) # visual_ refined output Size([192, 23, 512])
+
+            # CTC DECODER
+            top_prob, mid_prob, bot_prob  = self.CTC(visual_refined)
+
+            top_prob_list.append(top_prob)
+            mid_prob_list.append(mid_prob)
+            bot_prob_list.append(bot_prob)
         
         #Selective Contextual Refinement
         visual_feature_perm = visual_feature.permute(0, 3, 1, 2).squeeze(3)
@@ -162,19 +166,19 @@ class SCATTER(nn.Module):
 #         scr_probs_2, H = self.SCR_2(H, text, is_train)
 #         scr_probs_3, H = self.SCR_3(H, text, is_train)
 
+        ############################################
         bot_H1, bot_prob_list = self.SCR_bot_1(visual_feature_perm, visual_feature_perm, bot_prob_list, [text_bot], is_train)
         _, bot_prob_idx = bot_prob_list[-1].max(2)
         mid_H1, mid_prob_list = self.SCR_mid_1(visual_feature_perm, visual_feature_perm, mid_prob_list, [bot_prob_idx, text_mid], is_train)
         _, mid_prob_idx = mid_prob_list[-1].max(2)
         top_H1, top_prob_list = self.SCR_top_1(visual_feature_perm, visual_feature_perm, top_prob_list, [bot_prob_idx, mid_prob_idx, text_top], is_train)
-        
+        ##############################################
         bot_H2, bot_prob_list = self.SCR_bot_2(bot_H1, visual_feature_perm, bot_prob_list, [text_bot], is_train)
         _, bot_prob_idx = bot_prob_list[-1].max(2)
         mid_H2, mid_prob_list = self.SCR_mid_2(mid_H1, visual_feature_perm, mid_prob_list, [bot_prob_idx, text_mid], is_train)
         _, mid_prob_idx = mid_prob_list[-1].max(2)
         top_H2, top_prob_list = self.SCR_top_2(top_H1, visual_feature_perm, top_prob_list, [bot_prob_idx, mid_prob_idx, text_top], is_train)        
-        
-            
+        ##############################################
         bot_prob_list = self.SCR_bot_3(bot_H2, visual_feature_perm, bot_prob_list, [text_bot], is_train)    
         _, bot_prob_idx = bot_prob_list[-1].max(2)
         mid_prob_list = self.SCR_mid_3(mid_H2, visual_feature_perm, mid_prob_list, [bot_prob_idx, text_mid], is_train)
