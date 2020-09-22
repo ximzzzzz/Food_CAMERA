4069f67e3f5717ea60f948ecf04d5f010a40d22a
ximzzzzz <ximzzzzz@gmail.com>
2020-09-22 18:45:56 +0900

    additional lstm

--- RobustScanner/Hybrid.py
+++ RobustScanner/Hybrid.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-device = torch.device('cuda')
+# device = torch.device('cuda')
 
 
 class HybridBranch(nn.Module):
@@ -9,7 +9,8 @@ class HybridBranch(nn.Module):
     def __init__(self, hidden_size, num_steps, num_classes, device):
         super(HybridBranch, self).__init__()
         self.embedding = torch.nn.Linear(num_classes, hidden_size)
-        self.lstm = torch.nn.LSTMCell(hidden_size, hidden_size)
+        self.lstm = torch.nn.LSTMCell(hidden_size, hidden_size, bias=True)
+        self.lstm2 = torch.nn.LSTMCell(hidden_size, hidden_size, bias=True)
         self.generator = torch.nn.Linear(hidden_size, num_classes)
         self.num_steps = num_steps
         self.hidden_size = hidden_size
@@ -19,7 +20,7 @@ class HybridBranch(nn.Module):
     def forward(self, origin_fmap, text, is_train):
         batch_size = text.size(0)
         output_hiddens = torch.FloatTensor(batch_size, self.num_steps, self.hidden_size).fill_(0).to(self.device)
-        h_t, c_t = torch.FloatTensor(batch_size, self.hidden_size).to(self.device), torch.FloatTensor(batch_size, self.hidden_size).to(self.device)
+        h_t, c_t = torch.FloatTensor(batch_size, self.hidden_size).fill_(0).to(self.device), torch.FloatTensor(batch_size, self.hidden_size).fill_(0).to(self.device)
         origin_fmap_trans = origin_fmap.permute(0, 2, 3, 1).view(batch_size, -1, self.hidden_size)
 
         if is_train :
@@ -27,19 +28,23 @@ class HybridBranch(nn.Module):
                 # one-hot
                 x_t = torch.FloatTensor(batch_size, self.num_classes).zero_().to(self.device)
                 x_t = x_t.scatter_(1, text[:, i].unsqueeze(1), 1)
-                x_t_emb = embedding(x_t)
-                h_t, c_t = lstm(x_t_emb, (h_t, c_t))
-
+                x_t_emb = self.embedding(x_t)
+#                 print(f'x_t_emb shape : {x_t_emb.shape}')
+               
+                h_t, c_t = self.lstm(x_t_emb, (h_t, c_t))
+                h_t, c_t = self.lstm2(h_t)
+#                 print('origin fmap trans : ', origin_fmap_trans.shape)
+#                 print('h_t : ', h_t.unsqueeze(2).shape)
                 a = torch.softmax(torch.bmm(origin_fmap_trans, h_t.unsqueeze(2)), 1)
                 context = torch.bmm(a.permute(0, 2, 1), origin_fmap_trans)
                 output_hiddens[:, i, :] = context.squeeze(1)
-            g = generator(output_hiddens)
+            g = self.generator(output_hiddens)
 
         else:
             next_input = None
             g = torch.FloatTensor(batch_size, self.num_steps, self.num_classes).fill_(0).to(self.device)
 
-            for i in range(self.num_classes):
+            for i in range(self.num_steps):
                 if next_input==None:
                     x_t = torch.FloatTensor(batch_size, self.num_classes).zero_().to(self.device)
                     target = torch.LongTensor(batch_size, 1).fill_(0).to(self.device)
@@ -49,12 +54,12 @@ class HybridBranch(nn.Module):
                     x_t = torch.FloatTensor(batch_size, self.num_classes).zero_().to(self.device)
                     x_t = x_t.scatter_(1, next_input, 1)
 
-                x_t_emb = embedding(x_t)
+                x_t_emb = self.embedding(x_t)
                 a = torch.softmax(torch.bmm(origin_fmap_trans, h_t.unsqueeze(2)), 1)
                 context = torch.bmm(a.permute(0, 2, 1), origin_fmap_trans)
-                g_t = generator(context)
+                g_t = self.generator(context)
 
                 _, next_input = g_t.max(2)
                 g[:, i, :] = g_t.squeeze(1)
 
-            return g
+        return g
