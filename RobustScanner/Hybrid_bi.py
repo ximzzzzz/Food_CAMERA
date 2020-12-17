b66af6b32c1f22fde9af5bb4f974d67d3da22a96
ximzzzzz <ximzzzzz@gmail.com>
2020-12-17 18:56:52 +0900

    softmax dimension modified!!

--- RobustScanner/Hybrid_bi.py
+++ RobustScanner/Hybrid_bi.py
@@ -11,6 +11,7 @@ class HybridBranch(nn.Module):
         self.embedding = torch.nn.Linear(num_classes, hidden_size)
 #         self.lstm = torch.nn.LSTMCell(hidden_size, hidden_size, bias=True)
         self.lstm = torch.nn.LSTM(hidden_size, int(hidden_size/2), bidirectional=True, num_layers=2, batch_first=True)
+#         self.lstm = torch.nn.LSTM(hidden_size, hidden_size, bidirectional=False, num_layers=1, batch_first=True)
         self.generator = torch.nn.Linear(hidden_size, num_classes)
         self.num_steps = num_steps
         self.hidden_size = hidden_size
@@ -23,6 +24,10 @@ class HybridBranch(nn.Module):
         output_hiddens = torch.FloatTensor(batch_size, self.num_steps, self.hidden_size).fill_(0).to(self.device)
         states = (torch.FloatTensor(4, batch_size, int(self.hidden_size/2)).fill_(0).to(self.device), 
                   torch.FloatTensor(4, batch_size, int(self.hidden_size/2)).fill_(0).to(self.device))
+#         states = (torch.FloatTensor(2, batch_size, self.hidden_size).fill_(0).to(self.device), 
+#                   torch.FloatTensor(2, batch_size, self.hidden_size).fill_(0).to(self.device))
+#         states = (torch.FloatTensor(1, batch_size, self.hidden_size).fill_(0).to(self.device), 
+#                   torch.FloatTensor(1, batch_size, self.hidden_size).fill_(0).to(self.device))
         origin_fmap_trans = origin_fmap.permute(0, 2, 3, 1).view(batch_size, -1, self.hidden_size)
 
         if is_train :
@@ -35,7 +40,7 @@ class HybridBranch(nn.Module):
                 output, states = self.lstm(x_t_emb, states)
 #                 print('origin fmap trans : ', origin_fmap_trans.shape)
 #                 print('h_t : ', h_t.unsqueeze(2).shape)
-                a = torch.softmax(torch.bmm(output, origin_fmap_trans.permute(0, 2, 1)), 1)
+                a = torch.softmax(torch.bmm(output, origin_fmap_trans.permute(0, 2, 1)), 2) #shape (N, n_seq, dim), so softmax put effect on dim 2
                 context = torch.bmm(a, origin_fmap_trans)
                 output_hiddens[:, i, :] = context.squeeze(1)
             g = self.generator(output_hiddens)
@@ -57,7 +62,7 @@ class HybridBranch(nn.Module):
                 x_t_emb = self.embedding(x_t).unsqueeze(1)
                 self.lstm.flatten_parameters()
                 output, states = self.lstm(x_t_emb, states) ######################### additional lstm 
-                a = torch.softmax(torch.bmm(output, origin_fmap_trans.permute(0,2,1)), 1)
+                a = torch.softmax(torch.bmm(output, origin_fmap_trans.permute(0,2,1)), 2)
                 context = torch.bmm(a, origin_fmap_trans)
                 g_t = self.generator(context)
 
