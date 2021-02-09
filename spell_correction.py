d150239e29a6b5704333dce86e5208a925e2d0d6
ximz <ximzzzzz@gmail.com>
2021-02-09 23:32:28 +0900

    spell correction

--- spell_correction.py
+++ spell_correction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[37]:
+# In[1]:
 
 
 import pandas as pd
@@ -11,7 +11,7 @@ from soynlp.hangle import levenshtein, jamo_levenshtein
 import re
 
 
-# In[5]:
+# In[2]:
 
 
 home = '/home'
@@ -21,7 +21,7 @@ lexicon = pd.read_csv(lexicon_path)
 # orig_lexicon = lexicon['preprocess'][:2151]
 
 
-# In[13]:
+# In[3]:
 
 
 orig_lexicon = lexicon['preprocess']
@@ -71,28 +71,45 @@ for word in orig_lexicon:
         word_count_ed[0][word_splitted]
 
 
-# In[44]:
+# In[108]:
+
+
+word_count[0]
+
+
+# In[112]:
 
 
 # test ed search
-test_word = '카리'
+test_word = '아몬디'
 ed_list = {}
 for lexicon_word in word_count[0].keys():
-#     if levenshtein(test_word, lexicon_word)==1:
-#         print(lexicon_word)
-    ed = jamo_levenshtein(test_word, lexicon_word)
-    ed_list[lexicon_word] = round(ed,3)
+    if levenshtein(test_word, lexicon_word)==1:
+        print(lexicon_word)
+#     ed = jamo_levenshtein(test_word, lexicon_word)
+#     ed_list[lexicon_word] = round(ed,3)
     
-pd.DataFrame({'food' : list(ed_list.keys()), 'ed' : list(ed_list.values())}, index=range(len(ed_list))).sort_values(by='ed')['food'].values[0]
+# ed_df = pd.DataFrame({'food' : list(ed_list.keys()), 'ed' : list(ed_list.values())}, index=range(len(ed_list))).sort_values(by='ed')
+# min_ed = ed_df['ed'].values[0]
+# if min_ed > 0.667: #한글자만 다른경우 0.667 마지노선
+#     print('better to keep rather than correct')
+# else:
+#     corr = ed_df['food'].values[0]
 
 
-# In[67]:
+# In[113]:
 
 
 test_words = get_ed1_words(test_word)
 
 
-# In[69]:
+# In[114]:
+
+
+test_words
+
+
+# In[115]:
 
 
 wrong_words = []
@@ -101,36 +118,30 @@ for test_ in test_words:
         wrong_words.append(test_)
 
 
-# In[70]:
+# In[116]:
 
 
 wrong_words
 
 
-# In[71]:
+# In[117]:
 
 
 longest_word = sorted(wrong_words, key=lambda x : len(x), reverse=True )[0]
 
 
-# In[74]:
+# In[126]:
 
 
-longest_word
-
-
-# In[73]:
-
-
-length = len(longest_word)
+length = len(wrong_words)
 for char in longest_word:
-    have_char = [True if x.find(char)!=-1 else False for x in wrong_words]
+    have_char = [True if x.find(char)!=-1 else False for x in list(wrong_words)]
     if sum(have_char)==length:
         typo = char
 print(typo)
 
 
-# In[31]:
+# In[127]:
 
 
 typo_idx = test_word.index(typo)
@@ -139,24 +150,51 @@ post_word = test_word[typo_idx+1:]
 exception_word = pre_word + post_word
 
 
-# In[32]:
+# In[135]:
 
 
-exception_word
-
-
-# In[100]:
-
-
+ed_1 = []
 for product in word_count[0].keys():
     if levenshtein(exception_word, product) ==1:
-        correction_word = product
+        ed_1.append(product)
+#         correction_word = product
+jamo_ed_1 = {}
+for ed_ in ed_1:
+    jamo_ed = round(jamo_levenshtein(exception_word, ed_),2)
+    jamo_ed_1[ed_] = jamo_ed
 
 
-# In[101]:
+# In[136]:
 
 
-correction_word
+ed_1
+
+
+# In[137]:
+
+
+jamo_ed_1
+
+
+# In[188]:
+
+
+test_word = '농삼'
+ed_1 = []
+for lexicon_word in word_count[0].keys():
+    if levenshtein(test_word, lexicon_word)==1:
+        ed_1.append(lexicon_word)
+
+jamo_ed_1 = {}
+for ed_ in ed_1:
+    jamo_ed = round(jamo_levenshtein(test_word, ed_),4)
+    jamo_ed_1[ed_] = jamo_ed
+
+
+# In[189]:
+
+
+jamo_ed_1
 
 
 # In[128]:
@@ -199,6 +237,73 @@ def get_correction(test_word):
 
 
 get_correction('jawelfj')
+
+
+# In[5]:
+
+
+def get_factorial(product):
+    factorial = []
+    splitted_product = product.split('_')
+    if len(splitted_product)==1:
+        return [product]
+    for idx, splitted in enumerate(splitted_product):
+        forward_split = splitted_product[:idx]
+        backward_split = splitted_product[idx+1: ]
+        except_split = forward_split + backward_split
+        for split in except_split:
+            factorial.append(splitted+'_'+split)
+    return factorial
+
+
+# In[6]:
+
+
+bigram_lexicon = []
+for product in orig_lexicon:
+    facto = get_factorial(product)
+    bigram_lexicon = bigram_lexicon + facto
+
+
+# In[9]:
+
+
+bigram_lexicon = set(bigram_lexicon)
+
+
+# In[10]:
+
+
+# test ed search
+test_word = ['카리','3분']
+test_list_corr = '스팬_클래식'
+ed_list = []
+for lexicon_word in bigram_lexicon:
+    if levenshtein(test_list_corr, lexicon_word)==1:
+        ed_list.append(lexicon_word)
+
+least_ed = 1
+correction_name = test_list_corr
+# correction_list = {}
+for ed_nominated in ed_list:
+    jamo_ed = jamo_levenshtein(ed_nominated, test_list_corr)
+    if least_ed > jamo_ed:
+        least_ed = jamo_ed
+        correction_name = ed_nominated
+#     correction_list[ed_nominated] =jamo_ed
+correction_name
+
+
+# In[221]:
+
+
+list(bigram_lexicon).index('스팸_클래식')
+
+
+# In[196]:
+
+
+list(correction_list)
 
 
 # In[ ]:
